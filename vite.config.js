4d94e4f6ce139ca0ad98175b47cb46bbca897a07
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-03-19 19:31:55 +0300

    adding vite and image optimization

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+  plugins: [
+    ViteImageOptimizer({
+      jpg: {
+        quality: 70,
+      }
+    }),
+  ],
+});
