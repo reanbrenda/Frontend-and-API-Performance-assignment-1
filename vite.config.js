bd2a2b6d11c2a7d1c0203ee96df8729360c53371
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-03-19 21:36:01 +0300

    adding preload and lazyloading for images "

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,33 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 import path from 'path';
 
+const preloadCss = () => ({
+    name: "preload-assets",
+    transformIndexHtml(html) {
+      return html.replace(
+        /<\/head>/,
+        `<link rel='preload' href='./font.woff2' as='font' type='font/woff2' crossorigin='anonymous'>\n</head>`
+      );
+    }
+  });
+const asyncJs = () => ({
+  name: "async-js",
+  transformIndexHtml(html) {
+    return html.replace(/<script/, "<script async");
+  }
+});
+
+const lazyLoadImages = () => ({
+    name: "lazy-load-images",
+    transformIndexHtml(html, { filename }) {
+      if (filename !== 'index.html') {
+        return html.replace(/<img/, "<img loading='lazy'");
+      }
+      return html;
+    }
+  });
+  
+
 export default defineConfig({
   plugins: [
     ViteImageOptimizer({
@@ -17,10 +44,15 @@ export default defineConfig({
     ViteMinifyPlugin({
       html: true
     }),
-   
+    preloadCss(),
+    asyncJs(),
+    lazyLoadImages()
   ],
   base: './',
   build: {
+    css: {
+      include: ['style.min.css'],
+    },
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
